176d226bb7b02dcc4af5eb681de2898dbbf75003
unknown <bejolo018@gmail.com>
2019-05-06 15:31:57 -0500

    built out components necessary for comment section

---
+++ instagram/ig-app-week-two/src/Components/CommentSection/CommentInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CommentSection.css'
+
+const CommentInput = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Add comment... " />
+    </form>
+  );
+};
+
+export default CommentInput;
