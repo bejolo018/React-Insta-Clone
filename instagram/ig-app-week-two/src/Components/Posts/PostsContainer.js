2a6b106aef5e8407cddc31dc8f4df9518ee16a96
unknown <bejolo018@gmail.com>
2019-05-06 15:36:58 -0500

    built out posts and searchbar components

---
+++ instagram/ig-app-week-two/src/Components/Posts/PostsContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Post from './Post';
+import './PostContainer.css';
+
+
+const PostsContainer = props => {
+  return (
+    <div className="posts-container-wrapper">
+      {props.posts.map(p => <Post key={p.imageUrl} post={p} />)}
+      
+    </div>
+  );
+};
+
+
+
+
+export default PostsContainer;
